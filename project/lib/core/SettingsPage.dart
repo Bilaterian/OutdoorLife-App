0d5a46687a9b81c75b81f8806be4125cd2532ab2
Lance Paje <55113864+Bilaterian@users.noreply.github.com>
2022-03-05 16:03:50 -0500

    Updated Settings Page
    settings page now contains content

--- project/lib/core/SettingsPage.dart
+++ project/lib/core/SettingsPage.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget{
@@ -16,13 +18,81 @@ class SettingsPage extends StatelessWidget{
           ),
           title: const Text('Settings'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+                "Temperature",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                          'Celsius(°C)',
+                          style: TextStyle(
+                            color: Color(0xff262626),
+                          ),
+                          textAlign: TextAlign.left,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xffffffff)
+                      )
+                  ),
+                ),
+                Icon(Icons.check),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Farenheit(°F)',
+                        style: TextStyle(
+                          color: Color(0xff262626),
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xffffffff)
+                      )
+                  ),
+                ),
+                Icon(Icons.check),
+              ],
+            ),
+            Text(
+                "Restore to Default Settings",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff262626),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                      'Reset',
+                      style: TextStyle(
+                        color: Color(0xffffffff),
+                      )
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Color(0xff262626)
+                  )
+              ),
+            ),
+          ],
         ),
       ),
     );

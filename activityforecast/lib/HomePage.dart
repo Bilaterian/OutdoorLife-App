d5d692b186fdde500213d1000c767d83e473962d
Richard-Nguyen-UofG <richard_nguyen@live.ca>
2022-03-11 22:12:51 -0500

    conflict errors resolved

--- activityforecast/lib/HomePage.dart
+++ activityforecast/lib/HomePage.dart
@@ -4,6 +4,8 @@ import 'package:activityforecast/pages/SettingsPage.dart';
 import 'package:activityforecast/pages/manage_activities.dart';
 import 'package:activityforecast/view/pages/edit_activity_page.dart';
 
+import 'components/themes/manage_activities_colors.dart';
+
 class HomePage extends StatefulWidget {
 
   @override
@@ -29,23 +31,25 @@ class _HomePageState extends State<HomePage> {
       debugShowCheckedModeBanner: false,
       routes: <String, WidgetBuilder>{
         //"/": (BuildContext context) => MaterialApp(home: MyApp()),
-        "/ManageActivities": (BuildContext context) => MaterialApp(home: MainActivitiesPage(title: "Manage Activities")),
-        "/EditActivityPage": (BuildContext context) => MaterialApp(home: EditActivityPage()),
-        "/SettingsPage": (BuildContext context) => MaterialApp(home: SettingsPage()),
+        "/ManageActivities": (BuildContext context) =>MainActivitiesPage(title: "Manage Activities"),
+        "/EditActivityPage": (BuildContext context) => EditActivityPage(),
+        "/SettingsPage": (BuildContext context) => SettingsPage(),
       },
       home: Builder(
       builder: (context) =>
     //home: Scaffold(
       Scaffold(
+        backgroundColor: Color(0xff031342),
         resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text('OutdoorLife', style: TextStyle(color: Colors.black)),
+        backgroundColor: manageActivityColors['appBarColor'],
+        title: const Text('OutdoorLife', style: TextStyle(color: Color(0xff031342))),
         actions: [
           IconButton(
               onPressed: () {
                 Navigator.of(context).pushNamed("/SettingsPage");
               },
-              icon: Icon(Icons.settings, color: Colors.black))
+              icon: Icon(Icons.settings, color: Color(0xff031342)))
         ],
       ),
       body: Column(children: <Widget>[
@@ -62,9 +66,9 @@ class _HomePageState extends State<HomePage> {
               children: <Widget>[
                 activityBox(icon: Icons.directions_bike, text: "Bike", valid: Icons.check),
                 activityBox(icon: Icons.directions_boat, text: "Boat", valid: Icons.check),
-                activityBox(colour: Colors.deepPurple, icon: Icons.snowboarding, text: "Snowboard", valid: Icons.check),
+                activityBox(icon: Icons.snowboarding, text: "Snowboard", valid: Icons.check),
                 activityBox(icon: Icons.add_photo_alternate, text: "Photos", valid: Icons.clear_rounded),
-                activityBox(size1: 83.0, colour: Colors.deepPurple, icon: Icons.wash, text: "Clean", valid: Icons.check),
+                activityBox(size1: 83.0, icon: Icons.wash, text: "Clean", valid: Icons.check),
               ]
           ),
           Row(
@@ -94,52 +98,108 @@ class _HomePageState extends State<HomePage> {
           ),
 
           // Activity Forecast
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Center(
                   child: Container(
                     padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: Text('$selectedActivity' + " Forecast",
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-
+                    child: Text(
+                      '$selectedActivity' + " Forecast",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
                 TextButton(
-                  style: TextButton.styleFrom(primary: Colors.black),
-                  child: Text("EDIT", style: TextStyle(fontSize: 18)),
-                  onPressed: () {
-                    Navigator.of(context).pushNamed("/EditActivityPage");
-                  }
-                )
-              ]
-          ),
-          Row(children: forecastRow(day: "Today", valid: Icons.check, weather: Icons.wb_sunny, temperature: "16°,13° low")),
-          Row(children: forecastRow(day: "Fri", valid: Icons.clear_rounded, weather: Icons.wb_cloudy, temperature: "16°,13° low")),
-          Row(children: forecastRow(day: "Sat", valid: Icons.check, weather: Icons.wb_sunny, temperature: "16°,13° low")),
-          Row(children: forecastRow(day: "Sun", valid: Icons.clear_rounded, weather: Icons.wb_twilight, temperature: "16°,13° low")),
-          Row(children: forecastRow(day: "Mon", valid: Icons.check, weather: Icons.wb_sunny, temperature: "16°,13° low")),
-          Row(children: forecastRow(day: "Tue", valid: Icons.clear_rounded, weather: Icons.wb_sunny, temperature: "16°,13° low")),
-          Row(children: forecastRow(day: "Wed", valid: Icons.check, weather: Icons.wb_sunny, temperature: "16°,13° low")),
-        ])
-      ),
+                    style:
+                    TextButton.styleFrom(primary: Colors.black),
+                    child: Text("EDIT",
+                        style: TextStyle(
+                            fontSize: 18, color: Colors.white)),
+                    onPressed: () {
+                      //print("Edit Button Pressed");
+                      Navigator.of(context)
+                          .pushNamed("/EditActivityPage");
+                    })
+              ]),
+        ),
+        Row(
+            children: forecastRow(
+                day: "Today",
+                valid: Icons.check,
+                weather: Icons.wb_sunny,
+                temperature: "16°,13° low")),
+        Row(
+            children: forecastRow(
+                day: "Fri",
+                valid: Icons.clear_rounded,
+                weather: Icons.wb_cloudy,
+                temperature: "16°,13° low")),
+        Row(
+            children: forecastRow(
+                day: "Sat",
+                valid: Icons.check,
+                weather: Icons.wb_sunny,
+                temperature: "16°,13° low")),
+        Row(
+            children: forecastRow(
+                day: "Sun",
+                valid: Icons.clear_rounded,
+                weather: Icons.wb_twilight,
+                temperature: "16°,13° low")),
+        Row(
+            children: forecastRow(
+                day: "Mon",
+                valid: Icons.check,
+                weather: Icons.wb_sunny,
+                temperature: "16°,13° low")),
+        Row(
+            children: forecastRow(
+                day: "Tue",
+                valid: Icons.clear_rounded,
+                weather: Icons.wb_sunny,
+                temperature: "16°,13° low")),
+        Row(
+            children: forecastRow(
+                day: "Wed",
+                valid: Icons.check,
+                weather: Icons.wb_sunny,
+                temperature: "16°,13° low")),
+      ])),
     )
     );
   }
 
-  Widget activityBox({size1: 82.0, size2: 82.0, colour: Colors.deepPurple, icon: Icons.directions_walk, text: "Walk", valid: Icons.check}) {
+  Widget activityBox(
+      {size1: 82.0,
+        size2: 82.0,
+        // colour: Colors.deepPurple,
+        icon: Icons.directions_walk,
+        text: "Walk",
+        valid: Icons.check}) {
     return SizedBox.fromSize(
       size: Size(size1, size2), // button width and height
       child: Material(
-        color: colour, // button color
+        color: Colors.white, // button color
         child: InkWell(
           onTap: () => changeActivity(text),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Icon(icon), // icon
-              Text(text), // text
-              Icon(valid)
+              Icon(icon, color: Color(0xff3a0266)), // icon
+              Text(
+                text,
+              ), // text
+              Icon(
+                valid,
+                color: Color(0xff3a0266).withOpacity(.7),
+              )
             ],
           ),
         ),
@@ -147,36 +207,44 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  List<Widget> forecastRow({day: "Today", valid: Icons.check, weather: Icons.wb_sunny, temperature = "900°,900° low"}) {
+  List<Widget> forecastRow(
+      {day: "Today",
+        valid: Icons.check,
+        weather: Icons.wb_sunny,
+        temperature = "900°,900° low"}) {
     return <Widget>[
       Expanded(
           flex: 1,
           child: Container(
               width: 1440 * 0.25,
-              child: Text(day, textAlign: TextAlign.center, style: TextStyle(fontSize: 16))
-          )
-      ),
+              child: Text(day,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.white)))),
       Expanded(
           flex: 1,
           child: Container(
             width: 1440 * 0.25,
-            child: Icon(valid),
-          )
-      ),
+            child: Icon(
+              valid,
+              color: Colors.white,
+            ),
+          )),
       Expanded(
           flex: 1,
           child: Container(
             width: 1440 * 0.25,
-            child: Icon(weather),
-          )
-      ),
+            child: Icon(
+              weather,
+              color: Colors.white,
+            ),
+          )),
       Expanded(
           flex: 1,
           child: Container(
               width: 1440 * 0.25,
-              child: Text(temperature, textAlign: TextAlign.center, style: TextStyle(fontSize: 16))
-          )
-      ),
+              child: Text(temperature,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.white)))),
     ];
   }
 }

8b54a9004cb618c0c2535ea9f8fefaebe2060be0
Richard-Nguyen-UofG <richard_nguyen@live.ca>
2022-04-09 13:02:51 -0400

    improvements and base weather code

---
+++ activityforecast/lib/services/weather.dart
@@ -0,0 +1,57 @@
+import 'package:activityforecast/services/location.dart';
+import 'package:activityforecast/services/network.dart';
+
+const apiKey = 'fc6a05cad5b2a95ef99622d54d0f2828';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather'; //'https://api.openweathermap.org/data/2.5/onecall'; //'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getWeather(Location location) async {
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return 'thunder';
+    } else if (condition < 400) {
+      return 'drizzle';
+    } else if (condition == 500) {
+      return 'rain';
+    } else if (condition < 600) {
+      return 'heavy_rain';
+    } else if (condition < 700) {
+      return 'snow';
+    } else if (condition < 800) {
+      return 'fog';
+    } else if (condition == 800) {
+      return 'sun';
+    } else if (condition <= 804) {
+      return 'cloud';
+    } else {
+      return 'thermometer';
+    }
+  }
+}
--- activityforecast/lib/view/pages/home_page.dart
+++ activityforecast/lib/view/pages/home_page.dart
@@ -1,4 +1,6 @@
 // main.dart
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:activityforecast/pages/SettingsPage.dart';
 import 'package:activityforecast/pages/manage_activities.dart';
@@ -8,8 +10,15 @@ import 'package:provider/provider.dart';
 
 import '../../models/theme.dart';
 import '../../models/theme_provider.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../../services/weather.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
+  HomePage({this.locationWeather});
+
+  final locationWeather;
 
   late Color? activityContentsColor;
   late Color? backgroundColor;
@@ -28,8 +37,30 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  // colours
   late ColourScheme theme;
+  //late Position _currentPosition;
 
+  // city input
+  late final _searchController = TextEditingController();
+  String cityInput = "London";
+
+
+  // weather info
+  //late Location location;
+  WeatherModel weather = WeatherModel();
+  DateTime date = DateTime.now();
+
+  late int temperature;
+  late int temperatureMin;
+  late int temperatureMax;
+  late String weatherIcon;
+  late String cityName;
+  late String dayName = '';
+  late String weatherCondition;
+
+
+  // selected activity
   String selectedActivity = "Bike";
   changeActivity(String activity) {
     setState(() {
@@ -39,12 +70,74 @@ class _HomePageState extends State<HomePage> {
 
   int numActivitiesPerRow = 5;
   int numRows = 2;
+
+  @override
+  void initState() {
+    super.initState();
+
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData) {
+    setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        temperatureMin = 0;
+        temperatureMax = 0;
+        weatherIcon = 'Error';
+        cityName = '';
+        weatherCondition = '';
+        return;
+      }
+
+      /*
+      var temp = weatherData['current']['temp'];
+      temperature = temp.toInt();
+
+      var tempMin = weatherData['current']['humidity'];
+      temperatureMin = tempMin.toInt();
+
+      var tempMax = weatherData['current']['temp'];
+      temperatureMax = tempMax.toInt();
+
+      //var condition = weatherData['weather'][0]['id'];
+      //weatherIcon = weather.getWeatherIcon(condition);
+
+      cityName = weatherData['timezone'];
+
+      dayName = DateFormat('EEEE').format(date);
+
+      //weatherCondition = weatherData['weather'][0]['main'];
+       */
+
+      // current weather API (not one call)
+      var temp = weatherData['main']['temp'];
+      temperature = temp.toInt();
+
+      var tempMin = weatherData['main']['temp_min'];
+      temperatureMin = tempMin.toInt();
+
+      var tempMax = weatherData['main']['temp_max'];
+      temperatureMax = tempMax.toInt();
+
+      var condition = weatherData['weather'][0]['id'];
+      weatherIcon = weather.getWeatherIcon(condition);
+
+      cityName = weatherData['name'];
+
+      dayName = DateFormat('EEEE').format(date);
+
+      weatherCondition = weatherData['weather'][0]['main'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    // colours
     theme = Provider.of<ThemeProvider>(context).currentTheme;
 
     widget.activityContentsColor = theme.secondary;
-
     widget.backgroundColor = theme.secondary;
     widget.textColor = theme.quaternary;
     widget.boxColor = theme.quinary;
@@ -53,13 +146,56 @@ class _HomePageState extends State<HomePage> {
     widget.appBarIconColor = theme.secondary;
     widget.floatingButtonColor = theme.primary;
 
-
+    // dynamic sizing
     double screenWidth = MediaQuery.of(context).size.width;
-    double boxWidth = (screenWidth/numActivitiesPerRow).floorToDouble();
+    double boxWidth = screenWidth/numActivitiesPerRow;
+    //double boxWidthRounded = (screenWidth/numActivitiesPerRow).floorToDouble();
     double boxWidthIfExtraPixel = boxWidth;
-    if (boxWidth != screenWidth/numActivitiesPerRow) {
-      boxWidthIfExtraPixel = boxWidth + 1.0;
-    }
+
+    //if (boxWidthRounded != boxWidth) {
+      //boxWidthIfExtraPixel = boxWidthRounded + (boxWidth - boxWidthRounded);
+      //boxWidthIfExtraPixel = boxWidth + 1.0;
+    //}
+
+    // !<!<!< store position later for weather API
+    //Future<Position> location = _determinePosition();
+    //_determinePosition().then((value) => location1 = value as Location);
+
+    //Position pos = _determinePosition();
+    //getInitialLocation();
+
+    var kCityNameTextStyle = TextStyle(
+      fontFamily: 'OpenSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 48.0,
+      color: widget.textColor,
+    );
+
+    var kTimeTextStyle = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 32.0,
+      color: widget.textColor,
+    );
+
+    var kTemperatureTextStyle = TextStyle(
+      fontFamily: 'OpenSans',
+      fontWeight: FontWeight.bold,
+      fontSize: 80.0,
+      color: widget.textColor,
+    );
+
+    var kConditionTextStyle = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 24.0,
+      color: widget.textColor,
+    );
+
+    var kSmallTemperatureTextStyle = TextStyle(
+      fontFamily: 'OpenSans',
+      fontSize: 32.0,
+      color: widget.textColor,
+    );
+
 
     return MaterialApp(
         theme: ThemeData(scaffoldBackgroundColor: widget.backgroundColor),
@@ -87,17 +223,73 @@ class _HomePageState extends State<HomePage> {
                       icon: Icon(Icons.settings, color: widget.appBarIconColor))
                 ],
               ),
-              body: Column(children: <Widget>[
+              body: SingleChildScrollView(
+              child: Column(children: <Widget>[
+                /*
                 // Location Text Field
                 TextField(
                   style: TextStyle(color: widget.textColor),
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    hintText: 'Search Map',
+                    hintText: 'Enter City Name',
                     hintStyle: TextStyle(color: widget.textColor),
                   ),
                 ),
-
+                 */
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 50,
+                        child: TextField(
+                          controller: _searchController,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: widget.textColor, fontSize: 18),
+                          decoration: InputDecoration(
+                            fillColor: widget.backgroundColor,
+                            hintText: 'Enter City Name',
+                            hintStyle: TextStyle(color: widget.textColor, fontSize: 18),
+                            filled: true,
+                            border: OutlineInputBorder(
+                            ),
+                          ),
+                          onSubmitted: (value) async {
+                            print(value);
+                            cityInput = value;
+                            if (cityInput != null) {
+                              // !<!<!< var weatherData = await weather.getCityWeather(cityInput);
+                              // !<!<!< updateUI(weatherData);
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      child: Container(
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: widget.textColor,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                          child: Text('Search',
+                              style: TextStyle(fontSize: 18, color: widget.backgroundColor)),
+                        ),
+                      ),
+                      onTap: () async {
+                        FocusScope.of(context).unfocus();
+                        print(_searchController.text);
+                        cityInput = _searchController.text;
+                        if (cityInput != null) {
+                          // !<!<!< var weatherData = await weather.getCityWeather(cityInput);
+                          // !<!<!< updateUI(weatherData);
+                        }
+                      },
+                    )
+                  ],
+                ),
                 // Activities Validity
                 Row(
                     children: <Widget>[
@@ -154,10 +346,10 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                         TextButton(
-                            style: TextButton.styleFrom(primary: widget.textColor),
-                            child: Text("EDIT",
+                            style: TextButton.styleFrom(backgroundColor: widget.textColor),
+                            child: Text("Edit",
                                 style: TextStyle(
-                                    fontSize: 18, color: widget.textColor)),
+                                    fontSize: 18, color:  widget.backgroundColor)),
                             onPressed: () {
                               //print("Edit Button Pressed");
                               Navigator.of(context)
@@ -167,10 +359,10 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Row(
                     children: forecastRow(
-                        day: "Today",
+                        day: '$dayName',
                         valid: Icons.check,
-                        weather: Icons.wb_sunny,
-                        temperature: "16°,13° low")),
+                        weather: Icons.wb_sunny, // $weatherIcon or $weatherCondition?
+                        temperature: '$temperature°, $temperatureMin° low')),
                 Row(
                     children: forecastRow(
                         day: "Fri",
@@ -207,11 +399,79 @@ class _HomePageState extends State<HomePage> {
                         valid: Icons.check,
                         weather: Icons.wb_sunny,
                         temperature: "16°,13° low")),
+
+                /*
+                Container(
+                  alignment: Alignment.center,
+                  // constraints: BoxConstraints.expand(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              '$cityName',
+                              style: kCityNameTextStyle,
+                            ),
+                          ),
+                          Text(
+                            '$dayName',
+                            style: kTimeTextStyle,
+                          ),
+                        ],
+                      ),
+                      //Image.asset(
+                      //  'images/$weatherIcon.png',
+                      //  height: 160,
+                      //),
+                      Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(left: 25.0),
+                            child: Text(
+                              '$temperature°',
+                              style: kTemperatureTextStyle,
+                            ),
+                          ),
+                          Text(
+                            '$weatherCondition'.toUpperCase(),
+                            style: kConditionTextStyle,
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          //Image.asset(
+                          //  'images/thermometer_low.png',
+                          //  height: 50,
+                          //),
+                          Text(
+                            '$temperatureMin°',
+                            style: kSmallTemperatureTextStyle,
+                          ),
+                          //Image.asset(
+                          //  'images/thermometer_high.png',
+                          //  height: 50,
+                          //),
+                          Text(
+                            '$temperatureMax°',
+                            style: kSmallTemperatureTextStyle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                 */
+
                 Padding(
                   padding: const EdgeInsets.fromLTRB(0, 19, 0, 0),
                   child: Image.asset('assets/images/GoogleMapExample.jpg'),
                 )
-              ])),
+              ]))),
         )
     );
   }
@@ -241,11 +501,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  List<Widget> forecastRow(
-      {day: "Today",
-        valid: Icons.check,
-        weather: Icons.wb_sunny,
-        temperature = "900°,900° low"}) {
+  List<Widget> forecastRow({day: "Today", valid: Icons.check, weather: Icons.wb_sunny, temperature = "900°,900° low"}) {
     return <Widget>[
       Expanded(
           flex: 1,
@@ -281,4 +537,80 @@ class _HomePageState extends State<HomePage> {
                   style: TextStyle(fontSize: 16, color: widget.textColor)))),
     ];
   }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+    /*
+    return Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low).then((location) {
+      if (location != null) {
+        loc = location as Location;
+        //print("Location: ${location.latitude},${location.longitude}");
+        //locationRepository.store(location);
+      }
+      //return location;
+    }
+
+     */
+  }
+
+  void getInitialLocation() async {
+    //_determinePosition().then((value) => location1 = value as Location);
+    _determinePosition();
+
+    //var weatherData = await weather.getWeather(location1);
+    var weatherData = await weather.getLocationWeather();
+    // !<!<!< updateUI(weatherData);
+  }
+
+/*
+    getCurrentLocationAddress() async {
+      try {
+        List<Placemark> listPlaceMarks = await placemarkFromCoordinates(
+            _currentPosition.latitude, currentPosition.longitude);
+        Placemark place = listPlaceMarks[0];
+
+        setState(() {
+          currentLocationAddress = “${place.locality}, ${place.postalCode}, ${place.country}”;
+        }
+        );
+      } catch (e) {
+        print(e);
+      }
+    }
+     */
+
 }
